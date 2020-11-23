725f79c856bdf4bfd4aaa0d8e24172f1da694ae3
Rishita Mukherjee <rishitam2701@gmail.com>
2020-11-23 16:40:18 +0530

    21 done

--- app/src/main/java/flickster/shopping/design/ProductDetailsActivity.java
+++ app/src/main/java/flickster/shopping/design/ProductDetailsActivity.java
@@ -1,15 +1,19 @@
 package flickster.shopping.design;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import flickster.shopping.design.Model.Products;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,6 +21,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import flickster.shopping.design.Model.Products;
+import flickster.shopping.design.Prevalent.Prevalent;
 
 
 public class ProductDetailsActivity extends AppCompatActivity {
@@ -25,6 +35,8 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private ElegantNumberButton numberButton;
     private TextView productPrice ,productDescription ,productName;
     private String productID="";
+    private Button addToCartButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +49,60 @@ public class ProductDetailsActivity extends AppCompatActivity {
         productName=(TextView)findViewById(R.id.product_name_details);
         productDescription=(TextView)findViewById(R.id.product_description_details);
         productPrice=(TextView)findViewById(R.id.product_price_details);
-
+        addToCartButton=(Button)findViewById(R.id.pd_add_to_cart_button);
         getProductDetails(productID);
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addingToCartList();
+            }
+        });
     }
 
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+    saveCurrentDate=currentDate.format(calForDate.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime=currentDate.format(calForDate.getTime());
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        final HashMap<String,Object> cartMap = new HashMap<>();
+        cartMap.put("pid", productID);
+        cartMap.put("pname",productName.getText().toString());
+        cartMap.put("price", productPrice.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", numberButton.getNumber());
+        cartMap.put("discount", "");
+
+        cartListRef.child("User View")
+                .child(Prevalent.currentOnlineUser.getPhone())
+                .child("Products").child(productID).updateChildren(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            cartListRef.child("Admin View").child(Prevalent.currentOnlineUser.getPhone())
+                                    .child("Products").child(productID).updateChildren(cartMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            Toast.makeText(ProductDetailsActivity.this, "Added to Cart List", Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(ProductDetailsActivity.this, HomeActivity.this);
+                                            startActivity(intent);
+                                        }
+                                    });
+                        }
+
+                    }
+                });
+
+
+    }
     private void getProductDetails(String productID)
     {
         DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference().child("Products");
