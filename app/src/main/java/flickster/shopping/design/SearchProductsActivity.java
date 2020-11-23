8846c8e5d2e080964067c9fb9eef7e5a9927dace
Asus <nayniranaut@gmail.com>
2020-11-23 23:36:47 +0530

    34 done

--- app/src/main/java/flickster/shopping/design/SearchProductsActivity.java
+++ app/src/main/java/flickster/shopping/design/SearchProductsActivity.java
@@ -1,18 +1,32 @@
 package flickster.shopping.design;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import flickster.shopping.design.Model.Products;
+import flickster.shopping.design.ViewHolder.ProductViewHolder;
 
 public class SearchProductsActivity extends AppCompatActivity {
 
     private Button searchBtn;
     private EditText inputText;
     private RecyclerView searchList;
+    private String SearchInput;
 
 
     @Override
@@ -25,6 +39,52 @@ public class SearchProductsActivity extends AppCompatActivity {
         searchBtn = findViewById(R.id.search_btn);
         searchList = findViewById(R.id.search_list);
         searchList.setLayoutManager(new LinearLayoutManager(SearchProductsActivity.this));
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SearchInput =inputText.getText().toString();
+                onStart();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+        FirebaseRecyclerOptions<Products> options =new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(reference.orderByChild("pname").startAt(SearchInput),Products.class).build();
+
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int i, @NonNull final Products model) {
+                holder.txtProductName.setText(model.getPname());
+                holder.txtProductDescription.setText(model.getDescription());
+                holder.txtProductPrice.setText("Price = " + model.getPrice() + "$");
+                Picasso.get().load(model.getImage()).into(holder.imageView);
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent=new Intent(SearchProductsActivity.this,ProductDetailsActivity.class);
+                        intent.putExtra("pid",model.getPid());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+
+            }
+        };
+        searchList.setAdapter(adapter);
+        adapter.startListening();
 
     }
 }
